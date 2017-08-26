b36058fa8b8f6acdb1a36306334fb65de0294bd5
Dengelbrack <dengelbrack@protonmail.ch>
2017-08-26 20:00:01 +0200

    Add eslint exception
    Exception for arrow function with underscore notation (_ => ...). Would
    otherwise raise 'no-unused-vars' error.

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,16 @@ module.exports = {
         "semi": [
             "error",
             "always"
+        ],
+        "no-unused-vars":
+        [
+            "error",
+            {
+                "vars": "all",
+                "args": "after-used",
+                "ignoreRestSiblings": false,
+                "argsIgnorePattern": "^_$"
+            }
         ]
     }
 };
