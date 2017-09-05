146c41e9dbe29df4e203cd5d3fb2e40def65c5a5
Dengelbrack <dengelbrack@protonmail.ch>
2017-09-05 15:02:14 +0200

    Update unit tests for previous commit

---
+++ test/comp2.test.js
@@ -0,0 +1,14 @@
+const { comp2 } = require ("../prelude");
+
+const inc = x => x + 1;
+
+const add = x => y => x + y;
+
+test("comp2 (f) (g) (x) to equal f (g (x))", () => {
+    expect(comp2 (inc) (inc) (1)).toBe(inc (inc (1)));
+});
+
+test("comp2 (f) (g) (x) to equal f (g (x)) w/ partial application", () => {
+    expect(comp2 (add (1)) (add (1)) (1)).toBe(add (1) (add (1) (1)));
+});
+
