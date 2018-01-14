4f589879f6fc73f20d2877984d9f1524cc884916
Dengelbrack <dengelbrack@protonmail.ch>
2018-01-14 17:26:43 +0100

    Add monadic Writer

---
+++ src/control.monad.writer.js
@@ -0,0 +1,55 @@
+(function () {
+    "use strict";
+
+    const { Pair, append, show, eq } = require ("./prelude");
+
+    // data Writer
+    const Writer = function (x, ss) {
+        this.value = x;
+        this.writing = ss;
+        Object.freeze (this);
+    };
+
+    //    runWriter :: Writer a -> Pair a [String]
+    const runWriter = w => Pair (w.value) (w.writing);
+
+    //    execWriter :: Writer a -> [String]
+    const execWriter = w => w.writing;
+
+    //    writer :: (a, [String]) -> Writer a
+    const writer = (x, ss) => new Writer (x, ss);
+
+    //    tell :: [String] -> Writer ()
+    const tell = ss => new Writer (null, ss);
+
+    // instance Functor Writer
+    Writer.prototype.fmap = function (f) {
+        return writer (f (this.value), this.writing);
+    };
+
+    // instance Monad (Writer a)
+    Writer.pure = x => writer (x, [""]);
+    Writer.prototype.bind = function (f) {
+        const w2 = f (this.value);
+        return writer (w2.value, append (this.writing) (w2.writing));
+    };
+    Writer.prototype.then = function (w2) {
+        return writer (w2.value, append (this.writing) (w2.writing));
+    };
+
+    // instance Show a => Show (Writer a)
+    Writer.prototype.show = function () {
+        return "Writer { " + show (this.value) + ", " + show (this.writing) + " }";
+    };
+
+    // instance Eq a => Eq (Writer a)
+    Writer.prototype.eq = function (w2) {
+        return eq (this.value) (w2.value) && eq (this.writing) (w2.writing);
+    };
+
+    module.exports = {
+        Writer,
+        runWriter, execWriter,
+        writer, tell
+    };
+})();
