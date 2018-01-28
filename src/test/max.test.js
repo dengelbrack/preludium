18016d867d74ecb6986de4f31f0e81a111ffd160
Dengelbrack <dengelbrack@protonmail.ch>
2018-01-28 16:28:17 +0100

    Improve unit tests
    Branches were missing in ternary operator.

--- src/test/max.test.js
+++ src/test/max.test.js
@@ -4,3 +4,7 @@ test("max (1) (2) to be 2", () => {
     expect(max (1) (2)).toBe(2);
 });
 
+test("max (2) (1) to be 2", () => {
+    expect(max (2) (1)).toBe(2);
+});
+
--- src/test/min.test.js
+++ src/test/min.test.js
@@ -4,3 +4,7 @@ test("min (1) (2) to be 1", () => {
     expect(min (1) (2)).toBe(1);
 });
 
+test("min (2) (1) to be 1", () => {
+    expect(min (2) (1)).toBe(1);
+});
+
